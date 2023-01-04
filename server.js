8154a682f1b33dadb4a9ef1f6b42e34413f851e7
CharlieMickey <charles.f.mickey@gmail.com>
2023-01-03 20:52:39 -0600

    reset server code + add boilerplate

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { todos } from './data/todo-data.js';
 // Create Express app
 
 const app = express();
@@ -13,22 +12,6 @@ app.set('view engine', 'ejs');
 
 // Mount routes
 
-app.get('/', function(req,res){
-  res.send('<h1>Hey There Guy</h1>');
-})
-
-app.get('/waffles', function(req,res){
-  res.redirect('/home');
-})
-
-app.get('/home', function(req,res){
-  res.render('home');
-})
-app.get('/todos', function(req,res){
-  res.render('todos/index',{
-    todos,
-  })
-})
 
 // Tell the app to listen on port 3000
 
